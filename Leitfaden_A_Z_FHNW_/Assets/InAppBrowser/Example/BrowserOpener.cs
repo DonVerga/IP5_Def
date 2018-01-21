bd71ae88f28a85fa9fcc4432dad731b4fdd7d1de
DonVerga <fabian.becker@students.fhnw.ch>
2018-01-21 23:19:14 +0100

    added new Feature Vbutton and InappBrowser

--- Leitfaden_A_Z_FHNW_/Assets/InAppBrowser/Example/BrowserOpener.cs
+++ Leitfaden_A_Z_FHNW_/Assets/InAppBrowser/Example/BrowserOpener.cs
@@ -2,15 +2,14 @@
 using System.Collections;
 
 public class BrowserOpener : MonoBehaviour {
-     
 
 	public string pageToOpen = "http://www.google.com";
 
-    // check readme file to find out how to change title, colors etc.
-    public void OnButtonClicked() {
+	// check readme file to find out how to change title, colors etc.
+	public void OnButtonClicked() {
 		InAppBrowser.DisplayOptions options = new InAppBrowser.DisplayOptions();
 		options.displayURLAsPageTitle = false;
-		options.pageTitle = "AR-Application-Browser";
+		options.pageTitle = "InAppBrowser example";
 
 		InAppBrowser.OpenURL(pageToOpen, options);
 	}
---
+++ Leitfaden_A_Z_FHNW_/Assets/btnBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class btnBehavior : MonoBehaviour, IVirtualButtonEventHandler {
+
+    public GameObject vbBtnObj1;
+    public GameObject sphere;
+    
+
+	// Use this for initialization
+	void Start () {
+        vbBtnObj1 = GameObject.Find("SphereButton");
+        vbBtnObj1.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        sphere = GameObject.Find("Sphere");
+        sphere.SetActive(false);
+
+
+
+
+    }
+    public void OnButtonPressed( VirtualButtonBehaviour vb) {
+        sphere.SetActive(true);
+
+    }
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        sphere.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
